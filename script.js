cf282d72bacf9e5221c3b4e44c0abb5db22cadce
tutinjg <natijago@gmail.com>
2021-11-30 11:32:18 -0500

    Se agrega interaccion con el codigo de js y se realiza la transicion entre el formulario de inicio de sesion y el formulario de registro

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("btn_registrarse").addEventListener("click", register);
+document.getElementById("btn_iniciar-sesion").addEventListener("click", iniciarSesion);
+
+//Declaración de variables
+var contenedor_login_register = document.querySelector(".contenedor__login-register");
+var formulario_login = document.querySelector(".formulario__login");
+var formulario_register = document.querySelector(".formulario__register");
+var caja_trasera_login = document.querySelector(".caja__trasera-login");
+var caja_trasera_register = document.querySelector(".caja__trasera-register");
+
+function iniciarSesion(){
+    formulario_register.style.display = "none";
+    contenedor_login_register.style.left = "10px";
+    formulario_login.style.display = "block";
+    caja_trasera_register.style.opacity = "1";
+    caja_trasera_login.style.opacity = "0";
+}
+function register(){
+    formulario_register.style.display = "block";
+    contenedor_login_register.style.left = "410px";
+    formulario_login.style.display = "none";
+    caja_trasera_register.style.opacity = "0";
+    caja_trasera_login.style.opacity = "1";
+}
